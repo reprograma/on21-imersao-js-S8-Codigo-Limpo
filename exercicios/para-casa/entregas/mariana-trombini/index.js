38ca5b14bb616a8eda667faba702ce1c32b1063d
Mariana Trombini <mariana.trombini@dasa.com.br>
2022-11-08 08:41:33 -0300

    :sparkles: feat: add index with drink machine menu

---
+++ exercicios/para-casa/entregas/mariana-trombini/index.js
@@ -0,0 +1,38 @@
+const readline = require("readline-sync");
+const { listAllDrinks } = require('./listAllDrinks.js')
+const { listDrinkByIndex } = require('./listDrinkByIndex.js')
+
+const drinksMenu = () => {
+  console.log(`+------------+ Drink Machine Menu +------------+
+  1 - List all available drinks
+  2 - See drink details by index
+  3 - Exit
+  Choose an option between 1 and 3:
+  `);
+  const answer = readline.question();
+
+  console.clear();
+
+  switch (answer) {
+    case "1":
+      listAllDrinks();
+      drinksMenu();
+      break;
+
+    case "2":
+      listDrinkByIndex();
+      drinksMenu();
+      break;
+
+    case "3":
+      console.log("Thank you, come back soon!\n");
+      break;
+
+    default:
+      console.log(`You choose ${answer}. Invalid Option. Please try again!\n`);
+      drinksMenu();
+      break;
+  }
+}
+
+drinksMenu();
