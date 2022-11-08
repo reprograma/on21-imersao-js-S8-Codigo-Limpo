5b6e3c412cf5900dae93e20b33d51015d6545a9b
Valenthina Temudo <temudo.valenthina@gmail.com>
2022-11-08 19:19:38 -0300

    transformando arquivo em json e criando index.html

---
+++ exercicios/para-casa/entregas/valenthina-temudo/drinkMachine.js
@@ -0,0 +1,14 @@
+const drinks = require("./drinks.json");
+console.log(drinks);
+// function drinkMachine() {
+//   const list = drinks.map((item) => {
+//     return {
+//       name: item.name,
+//       price: item.price,
+//       brand: item.brand,
+//       flavor: item.flavor,
+//     };
+//   });
+
+//   console.log(list);
+// }
