0fda5551192729246e625cd430943104d6cbcc81
rosanaemidio <rosanaemidio42@gmail.com>
2022-11-10 23:50:49 -0300

    feat: add array de drinks

---
+++ exercicios/para-casa/entregas/rosana-emidio/drinks.js
@@ -0,0 +1,39 @@
+const drinks = [
+    {
+      id: 1,
+      name: "Water",
+      price: 1.5,
+      brand: "Lindóia",
+      flavor: "água sem gás",
+    },
+    {
+      id: 2,
+      name: "Wine",
+      price: 27.50,
+      brand: "Reservado",
+      flavor: "Rosé",
+    },
+    {
+      id: 3,
+      name: "Wine",
+      price: 37.91,
+      brand: "Almadén",
+      flavor: "White",
+    },
+    {
+      id: 4,
+      name: "Gin",
+      price: 128.53,
+      brand: "Beefeator",
+      flavor: "Pink",
+    },
+    {
+      id: 5,
+      name: "Beer",
+      price: 20.95,
+      brand: "Faxe",
+      flavor: "Witbier",
+    },
+]
+
+module.exports = drinks;
