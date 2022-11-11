a6a5bd33817ec41ad95ff4dae88151b0bc04978d
Rosalyne Duarte <rosalyneduarte@gmail.com>
2022-11-11 16:18:32 -0300

    Entrega de exercicio

---
+++ rosalyne-duarte/exercicios/para-casa/entregas/rosalyne-duarte/listaBebidas.js
@@ -0,0 +1,58 @@
+
+const listaBebidas = [
+  {
+    id: "0",
+    nome: "Água Mineral",
+    preco: 1.85,
+    marca: "Indaiá",
+    sabor: "Sem Gás",
+  },
+  {
+    id: "1",
+    nome: "Café Expresso",
+    preco: 5.50,
+    marca: "Três Corações",
+    sabor: "Forte",
+  },
+  {
+    id: "2",
+    nome: "Suco de Caixa",
+    preco: 8.50,
+    marca: "Del Valle",
+    sabor: "Uva",
+  },
+  {
+    id: "3",
+    nome: "Cachaça",
+    preco: 12.50,
+    marca: "Santa Dose",
+    sabor: "Mel e Limão",
+  },
+
+  {
+    id: "4",
+    nome: "Vodka",
+    preco: 52.50,
+    marca: "Diageo",
+    sabor: "Smirnoff",
+  },
+  {
+    id: "5",
+    nome: "Cerveja",
+    preco: 5.80,
+    marca: "Heineken",
+    sabor: "Puro Malte",
+  },
+  {
+    id: "6",
+    nome: "Caipirinha",
+    preco: 12.50,
+    marca: "Pitú Gold",
+    sabor: "Limão",
+  }
+]
+
+module.exports =  listaBebidas ;
+
+
+
