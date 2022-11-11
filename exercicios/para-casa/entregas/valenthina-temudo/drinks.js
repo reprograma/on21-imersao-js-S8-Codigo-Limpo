808abcc612c47c45a5e020a345d68fe2ee7d0872
Valenthina Temudo <temudo.valenthina@gmail.com>
2022-11-10 22:05:49 -0300

    enviando exercicio código limpo

---
+++ exercicios/para-casa/entregas/valenthina-temudo/drinks.js
@@ -0,0 +1,46 @@
+const drinks = [
+  {
+    id: 1,
+    name: "Water",
+    price: 2.5,
+    brand: "indaiá",
+    flavor: "água sem gás",
+  },
+  {
+    id: 2,
+    name: "Coffee",
+    price: 4.2,
+    brand: "Starbucks",
+    flavor: "Caramel Macchiato",
+  },
+  {
+    id: 3,
+    name: "Coffee",
+    price: 3.1,
+    brand: "Starbucks",
+    flavor: "Espresso",
+  },
+  {
+    id: 4,
+    name: "Soda",
+    price: 4.75,
+    brand: "Coca-cola",
+    flavor: "No sugar",
+  },
+  {
+    id: 5,
+    name: "Soda",
+    price: 4.75,
+    brand: "Coca-cola",
+    flavor: "Origin",
+  },
+  {
+    id: 6,
+    name: "Beer",
+    price: 5.1,
+    brand: "Budweiser",
+    flavor: "Origin",
+  },
+];
+
+module.exports = drinks;
