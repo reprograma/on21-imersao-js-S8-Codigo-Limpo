2333b24de97ea7edad9d7e315f9beefc578c7f01
Ana Paula <anapaula.lange@gmail.com>
2022-11-19 23:11:00 -0300

    ajustes atividade 8

--- exercicios/para-casa/entregas/anapaula-langegomes/resolucao.js
+++ exercicios/para-casa/entregas/anapaula-langegomes/resolucao.js
@@ -1,27 +1,42 @@
-const listaDeBebidas = [
+const  listaBebidas  = require('./listaBebidas.js')
+const entrada = require("prompt-sync")({sigint: true})
 
-];
+let finalizar = false;
 
-function escolherBebidas(nomeDaBebida) {
-  if (nomeBebida === "Água") {
-    return listaDeBebidas[0];
-  } else if (nomeDaBebida === "Suco") {
-    return listaDeBebidas[1];
-  } else if (nomeDaBebida === "Chá") {
-    return listaDeBebidas[2];
-  } else if (nomeDaBebida === "Vinho") {
-    return listaDeBebidas[3];
-  } else if (nomeDaBebida === "Refrigerante") {
-    return listaDeBebidas[4];
-  } else if (nomeDaBebida === "Soda") {
-    return listaDeBebidas[5];
-  } else {
-    throw Error("Essa bebida não está disponível!");
-  }
+function imprimirListaBebidas() {
+    console.log("\n Bebidas Disponiveis: \n");
+    if (listaBebidas.length > 0) {
+        for (let i = 0; i < listaBebidas.length; i++) {
+            console.log(`Bebida ${i + 1}: ${listaBebidas[i].nome}`);
+        }
+        console.log("\n");
+    } else {
+        console.log("Essa bebida não está disponível disponível.");
+    }
 }
 
-function menuBebidas() {
-
+function mostrarDetalhesBebida(id) {
+    console.log("\n Bebida selecionada");
+    console.log(
+        ` ${id}:  
+        Nome:`, listaBebidas[id - 1].nome,`; 
+        Preço:`, listaBebidas[id - 1].preco,`; 
+        Marca:`, listaBebidas[id - 1].marca,`; 
+        Sabor:`, listaBebidas[id - 1].sabor
+    );
 }
 
-menuBebidas();
+do {
+
+    let opcao = entrada("** Máquina de Bebidas ** \n Digite a opção Desejada:  \n 1 - Listar bebidas; \n 2 - Mostrar Detalhes Bebida; \n 3 - Sair \n");
+    switch (opcao) {
+        case "1": imprimirListaBebidas(); break;
+        case "2": {
+            let id = entrada("Digite o número da bebida escolhida:");
+            mostrarDetalhesBebida(id);
+        }; break;
+        case "3": sair = true; break
+        default: console.log("Favor digitar uma opção válida!");
+    }
+
+} while (!finalizar);
